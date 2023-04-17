Lasso regression (feature selection) via sklearn. PCA fixes (e.g., data scaling and filenames).

neural_networks/models.py
@@ -8,6 +8,7 @@ from tensorflow.keras.layers import Dense, Activation
 from utils.constants import SPCAM_Vars
 from utils.variable import Variable_Lev_Metadata
 import utils.pcmci_aggregation as aggregation
+from neural_networks.sklearn_lasso import sklasso
 
 
 class ModelDescription:
@@ -81,6 +82,8 @@ class ModelDescription:
         if setup.do_pca_nn: 
             setup.inputs_pca          = self.inputs_pca = self.inputs[:int(setup.n_components)]
             setup.input_pca_vars_dict = self.input_pca_vars_dict = ModelDescription._build_vars_dict(setup.inputs_pca)
+        
+        if setup.do_sklasso_nn: self.lasso_coefs = setup.lasso_coefs
 
         self.model = self._build_model()
         
@@ -320,10 +323,66 @@ def generate_all_causal_single_nn(setup, aggregated_results):
             var_names = np.array(pc_alpha_results["var_names"])
             for threshold, parent_idxs in pc_alpha_results["parents"].items():
                 parents = var_names[parent_idxs]
+                # print(f"output: {output}")
+                # print(f"parents: {parents}")
+                # print(f"var_names[parent_idxs]: {var_names[parent_idxs]}")
+                # print(f"parent_idxs: {parent_idxs}")
                 model_description = ModelDescription(
                     output, parents, setup.nn_type, pc_alpha, threshold, setup=setup,
                 )
                 model_descriptions.append(model_description)
+    return model_descriptions
+
+
+def generate_sklasso_single_nn(setup):
+    """ 
+    sklassoNN: Generate all NN with one output and Lasso (L1) as inputs
+    """
+    model_descriptions = list()
+
+    inputs = list()
+    for spcam_var in setup.spcam_inputs:
+        if spcam_var.dimensions == 3:
+            for level, _ in setup.parents_idx_levs:
+                # There's enough info to build a Variable_Lev_Metadata list
+                # However, it could be better to do a bigger reorganization
+                var_name = f"{spcam_var.name}-{round(level, 2)}"
+                inputs.append(var_name)
+        elif spcam_var.dimensions == 2:
+            var_name = spcam_var.name
+            inputs.append(var_name)
+    inputs = sorted(
+        [Variable_Lev_Metadata.parse_var_name(p) for p in inputs],
+        key=lambda x: setup.input_order_list.index(x),
+    )
+            
+    output_list = list()
+    for spcam_var in setup.spcam_outputs:
+        if spcam_var.dimensions == 3:
+            for level, _ in setup.children_idx_levs:
+                # There's enough info to build a Variable_Lev_Metadata list
+                # However, it could be better to do a bigger reorganization
+                var_name = f"{spcam_var.name}-{round(level, 2)}"
+                output_list.append(var_name)
+        elif spcam_var.dimensions == 2:
+            var_name = spcam_var.name
+        output_list.append(var_name)
+    
+    for output in output_list:
+        output = Variable_Lev_Metadata.parse_var_name(output)
+        lasso_inputs, lasso_coefs = sklasso(
+            inputs=inputs,
+            output=output,
+            data_fn=Path(setup.train_data_folder, setup.train_data_fn),
+            norm_fn=Path(setup.normalization_folder, setup.normalization_fn),
+            setup=setup,
+        )
+        setup.lasso_coefs = lasso_coefs
+        print(f"lasso_inputs: {lasso_inputs}")
+        model_description = ModelDescription(
+            output, lasso_inputs, setup.nn_type, pc_alpha=None, threshold=None, setup=setup,
+        )
+        model_descriptions.append(model_description)
     return model_descriptions
 
 
@@ -356,7 +415,10 @@ def generate_models(setup, threshold_dict=False):
         model_descriptions.extend(
             generate_all_causal_single_nn(setup, aggregated_results)
         )
-
+        
+    if setup.do_sklasso_nn:
+        model_descriptions.extend(generate_sklasso_single_nn(setup))
+        
     return model_descriptions
 
 

neural_networks/pca.py
@@ -1,7 +1,7 @@
 import xarray as xr
 from sklearn.decomposition import PCA
 from joblib import dump, load
-#from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 
 import numpy as np
 from pathlib import Path
@@ -15,7 +15,6 @@ def pca(
     input_vars_dict,
     norm_fn,
     load_pca_model,
-    # save_dir,
     setup,
 ):
     
@@ -33,22 +32,25 @@ def pca(
     
     # Normalize
     print(f"... normalizing data...")
-    sub, div = pca_norm(input_vars_dict, var_idxs, norm_ds, setup)
-    X_norm = (X - sub) / div
+    X_scaler=StandardScaler()
+    X_scaler.fit(X)
+    X_norm = X_scaler.transform(X)
     
     # PCA
     if load_pca_model:
-        print(f"... loading pca model...")
-        pca_fn = Path(setup.train_data_folder, setup.train_data_fn.split('.')[0]+f"_pca{setup.n_components}"+".joblib")
+        pca_fn = Path(setup.train_data_folder, setup.train_data_fn.split('.')[0]+".joblib")
+        print(f"... loading pca model: {pca_fn}...")
         pca = load(pca_fn)
     else:
-        print(f"... creating pca model...")
         pca_fn = Path(
             setup.train_data_folder, 
-            setup.train_data_fn.split('.')[0]+f"_pca{setup.n_components}"+".joblib"
+            # setup.train_data_fn.split('.')[0]+f"_pca{setup.n_components}"+".joblib"
+            setup.train_data_fn.split('.')[0]+"_pca"+".joblib"
         )
+        print(f"... creating pca model: {pca_fn}...")
         n_components = float(setup.n_components) if setup.n_components < 1. else int(setup.n_components)
-        pca = PCA(n_components=n_components)
+        # pca = PCA(n_components=n_components)
+        pca = PCA(n_components=num_inputs)
         pca.fit(X_norm)
         print(f"... saving pca model: {pca_fn}...")
         dump(pca, pca_fn)
@@ -56,14 +58,6 @@ def pca(
     
     # Save data
     print(f"... saving PC-components...")
-    # var_names = []; count = 1
-    # for i in range(num_inputs):
-    #     if count <= PCs.shape[-1]: 
-    #         var_names.append('PC'+str(count))
-    #     else:
-    #         var_names.append('null')
-    #     count += 1
-    # var_concat = np.concatenate((np.array(var_names),data_ds.coords['var_names'][num_inputs:]))
     zero_inputs = np.zeros([len(PCs[:,0]),num_inputs-len(PCs[0])])
     vars_data = np.concatenate((PCs,zero_inputs,Y),axis=1)
     
@@ -81,7 +75,11 @@ def pca(
     attrs = {'author':'Fernando Iglesias-Suarez', 
              'email':'fernando.iglesias-suarez@dlr.de',
              'Num. PCs':len(PCs[0]),
-             'explained_variance_ratio_.cumsum':pca.explained_variance_ratio_.cumsum()}
+             'explained_variance_ratio_.sum':pca.explained_variance_ratio_.sum(),
+             'explained_variance_ratio_':pca.explained_variance_ratio_,
+             'explained_variance_ratio_.cumsum':pca.explained_variance_ratio_.cumsum(),
+             'explained_variance_':pca.explained_variance_
+            }
 
     # create dataset
     pca_data_ds = xr.Dataset(data_vars=data_vars, coords=coords, attrs=attrs)
@@ -90,34 +88,3 @@ def pca(
     
     norm_ds.close()
     data_ds.close()
-    
-    
-def pca_norm(
-    input_vars_dict,
-    var_idxs,
-    norm_ds,
-    setup,
-):
-    
-    div = setup.input_div
-    sub = norm_ds[setup.input_sub].values[var_idxs]
-    
-    if div == "maxrs":
-        rang = norm_ds["max"][var_idxs] - norm_ds["min"][var_idxs]
-        std_by_var = rang.copy()
-        for v in input_vars_dict.keys():
-            std_by_var[std_by_var.var_names == v] = norm_ds["std_by_var"][
-                norm_ds.var_names_single == v
-            ]
-        div = np.maximum(rang, std_by_var).values
-    elif div == "std_by_var":
-        # SR: Total mess. Should be handled better
-        tmp_var_names = norm_ds.var_names[var_idxs]
-        div = np.zeros(len(tmp_var_names))
-        for v in input_vars_dict.keys():
-            std_by_var = norm_ds["std_by_var"][norm_ds.var_names_single == v]
-            div[tmp_var_names == v] = std_by_var
-    else:
-        div = norm_ds[div].values[var_idxs]
-    
-    return sub, div