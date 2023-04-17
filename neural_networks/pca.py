e190439dc70565b6ee52792f7e69babafffc1607
b309172 <Fernando.Iglesias-Suarez@dlr.de>
2023-04-17 13:29:08 +0200

    PCA small fix (data processing).

--- neural_networks/pca.py
+++ neural_networks/pca.py
@@ -20,25 +20,30 @@ def pca(
     
     # Open datasets
     data_ds = xr.open_dataset(data_fn)
+    # print(f"data_fn: {data_fn}")
     norm_ds = xr.open_dataset(norm_fn)
     
     # inputs idxs (all)
     var_idxs   = return_var_idxs(norm_ds, input_vars_dict)
+    # print(f"var_idxs: {var_idxs}")
     num_inputs = len(var_idxs)
     
     # Input (X) & output (Y) data
     X = data_ds["vars"][:,var_idxs]
+    # print(f"X: {X}")
     Y = data_ds["vars"][:,-(len(data_ds.var_names)-len(var_idxs)):]
+    # print(f"Y: {Y}")
     
     # Normalize
     print(f"... normalizing data...")
     X_scaler=StandardScaler()
     X_scaler.fit(X)
     X_norm = X_scaler.transform(X)
+    # print(f"X_norm.shape: {X_norm.shape}")
     
     # PCA
     if load_pca_model:
-        pca_fn = Path(setup.train_data_folder, setup.train_data_fn.split('.')[0]+".joblib")
+        pca_fn = Path(setup.train_data_folder, setup.train_data_fn.split('.')[0]+"_pca"+".joblib")
         print(f"... loading pca model: {pca_fn}...")
         pca = load(pca_fn)
     else:
@@ -50,16 +55,20 @@ def pca(
         print(f"... creating pca model: {pca_fn}...")
         n_components = float(setup.n_components) if setup.n_components < 1. else int(setup.n_components)
         # pca = PCA(n_components=n_components)
+        # print(f"num_inputs: {num_inputs}")
         pca = PCA(n_components=num_inputs)
         pca.fit(X_norm)
         print(f"... saving pca model: {pca_fn}...")
         dump(pca, pca_fn)
     PCs = pca.transform(X_norm)
+    print(F"PCs.shape:{PCs.shape}")
     
     # Save data
     print(f"... saving PC-components...")
-    zero_inputs = np.zeros([len(PCs[:,0]),num_inputs-len(PCs[0])])
-    vars_data = np.concatenate((PCs,zero_inputs,Y),axis=1)
+    # zero_inputs = np.zeros([len(PCs[:,0]),num_inputs-len(PCs[0])])
+    # vars_data = np.concatenate((PCs,zero_inputs,Y),axis=1)
+    vars_data = np.concatenate((PCs,Y),axis=1)
+    # print(f"vars_data.shape: {vars_data.shape}")
     
     # define data with variable attributes
     data_vars = {
