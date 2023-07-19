8ec046fbaed60ebace7f5176520b61cf3592133a
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2023-07-19 15:39:02 +0200

    Update slurm script with gpu and constraint options

--- train_castle_split_nodes_batch.sh
+++ train_castle_split_nodes_batch.sh
@@ -5,12 +5,13 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=90
+#SBATCH --gpus=4
 #SBATCH --mem=0
-#SBATCH --prefer=a100_80
+#SBATCH --constraint=a100_80
 #SBATCH --exclusive
 #SBATCH --time=12:00:00
 #SBATCH --account=bd1179
-#SBATCH --output=output_castle/training_4/castle_training_split_slurm.%j.out
+#SBATCH --output=output_castle/training_7_mirrored/castle_training_split_slurm_again.%j.out
 
 
 echo "Starting job:"`date`
@@ -20,5 +21,5 @@ echo "Starting job:"`date`
 #  $2 NN .txt inputs file
 #  $3 NN .txt outputs file
 #  $4 training indices
-conda run -n tensorflow_env python -u main_train_castle_split_nodes.py -c "$1" -i "$2" -o "$3" -x "$4" > "output_castle/training_4/castle_training_split_python_$SLURM_JOB_ID.out"
+conda run -n tensorflow_env python -u main_train_castle_split_nodes.py -c "$1" -i "$2" -o "$3" -x "$4" > "output_castle/training_7_mirrored/castle_training_split_python_again_$SLURM_JOB_ID.out"
 
