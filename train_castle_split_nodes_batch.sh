6a01947175651fa6d91b7ee0409c274f61d3898e
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2023-08-01 13:05:41 +0200

    Update config/batch script for new training runs

--- train_castle_split_nodes_batch.sh
+++ train_castle_split_nodes_batch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=castle_test_training
+#SBATCH --job-name=castle_training
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -11,7 +11,7 @@
 #SBATCH --exclusive
 #SBATCH --time=12:00:00
 #SBATCH --account=bd1179
-#SBATCH --output=output_castle/training_7_mirrored/castle_training_split_slurm_again.%j.out
+#SBATCH --output=output_castle/training_9_mirrored/castle_training_split_slurm.%j.out
 
 
 echo "Starting job:"`date`
@@ -21,5 +21,5 @@ echo "Starting job:"`date`
 #  $2 NN .txt inputs file
 #  $3 NN .txt outputs file
 #  $4 training indices
-conda run -n tensorflow_env python -u main_train_castle_split_nodes.py -c "$1" -i "$2" -o "$3" -x "$4" > "output_castle/training_7_mirrored/castle_training_split_python_again_$SLURM_JOB_ID.out"
+conda run -n tensorflow_env python -u main_train_castle_split_nodes.py -c "$1" -i "$2" -o "$3" -x "$4" > "output_castle/training_9_mirrored/castle_training_split_python_$SLURM_JOB_ID.out"
 
