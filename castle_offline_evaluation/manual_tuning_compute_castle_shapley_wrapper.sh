c8afb50e6b2e724ae39f4ad8b53245f14f64ddad
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2023-11-06 13:46:42 +0100

    Change wrappers to run for all model types

--- castle_offline_evaluation/manual_tuning_compute_castle_shapley_wrapper.sh
+++ castle_offline_evaluation/manual_tuning_compute_castle_shapley_wrapper.sh
@@ -44,8 +44,9 @@ lambda_sparsity=(0.1 0.5 1.0)
 PROJECT_ROOT="$(dirname "${PWD}")"
 
 # Set base directory for config files and inputs/outputs list files
-base_dir="${PROJECT_ROOT}/output_castle/manual_tuning_tphystnd_691.39_v2"
-tuning_model="castle_adapted_big_dagma"
+base_dir="${PROJECT_ROOT}/output_castle/manual_tuning_tphystnd_691.39_v4"
+trial="trial_2"
+tuning_models=("castle_adapted_small_dagma" "castle_adapted_big_dagma" "castle_adapted_small_notears" "castle_adapted_big_notears")
 in="${base_dir}/inputs_list.txt"
 out="${base_dir}/outputs_list.txt"
 map="${base_dir}/outputs_map.txt"
@@ -65,22 +66,24 @@ metric="all"
 # Start jobs #
 ##############
 
-for p in "${lambda_prediction[@]}"; do
-  for s in "${lambda_sparsity[@]}"; do
-    dir="lambda_pred_${p}-lambda_sparsity_${s}"
-    job_name="compute_shap_tphystnd_691_${tuning_model}_la_pred-${p}_la_sparsity-${s}"
+for tuning_model in "${tuning_models[@]}"; do
+  for p in "${lambda_prediction[@]}"; do
+    for s in "${lambda_sparsity[@]}"; do
+      dir="lambda_pred_${p}-lambda_sparsity_${s}"
+      job_name="compute_shap_tphystnd_691_${tuning_model}_la_pred-${p}_la_sparsity-${s}"
 
-    log_dir="${base_dir}/${tuning_model}/${dir}"
-    plot_dir="${base_dir}/evaluation/${tuning_model}/${dir}/shap"
+      log_dir="${base_dir}/${trial}/${tuning_model}/${dir}"
+      plot_dir="${base_dir}/${trial}/evaluation/${tuning_model}/${dir}/shap"
 
-    mkdir -p "$plot_dir"
-    cfg="${log_dir}/cfg_castle_adapted.yml"
+      mkdir -p "$plot_dir"
+      cfg="${log_dir}/cfg_castle_adapted.yml"
 
-    slurm_o="${plot_dir}/%x_slurm_%j.out"
-    slurm_e="${plot_dir}/%x_error_slurm_%j.out"
+      slurm_o="${plot_dir}/%x_slurm_%j.out"
+      slurm_e="${plot_dir}/%x_error_slurm_%j.out"
 
-    echo -e "\nSubmitting job ${job_name}"
-    sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_compute_castle_shapley.sh -c "$cfg" -o "$out" -x $idx -m "$map" -p "$plot_dir" -t "$n_time" -s "$n_samples" -e "$metric" -l "$plot_dir" -j "$job_name"
+      echo -e "\nSubmitting job ${job_name}"
+      sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_compute_castle_shapley.sh -c "$cfg" -o "$out" -x $idx -m "$map" -p "$plot_dir" -t "$n_time" -s "$n_samples" -e "$metric" -l "$plot_dir" -j "$job_name"
+    done
   done
 done
 echo ""
--- manual_tuning_wrapper.sh
+++ manual_tuning_wrapper.sh
@@ -38,27 +38,32 @@ lambda_prediction=(1 2 4 8 10)
 lambda_sparsity=(0.1 0.5 1.0)
 
 # Set base directory for config files and inputs/outputs list files
-base_dir="output_castle/manual_tuning_tphystnd_691.39_v4"
-tuning_model="castle_adapted_small_dagma"
+base_dir="output_castle/manual_tuning_tphystnd_691.39_v3"
+trial="trial_2"
+tuning_models=("castle_adapted_small_dagma" "castle_adapted_big_dagma" "castle_adapted_small_notears" "castle_adapted_big_notears")
 in="${base_dir}/inputs_list.txt"
 out="${base_dir}/outputs_list.txt"
 
 # Index for network to be tuned: variable tphystnd-691.39
 idx="20-20"
 
-for p in "${lambda_prediction[@]}"; do
-  for s in "${lambda_sparsity[@]}"; do
-    dir="lambda_pred_${p}-lambda_sparsity_${s}"
-    job_name="manual_tuning_tphystnd_691_${tuning_model}_lambda_pred_${p}-lambda_sparsity_${s}"
+for tuning_model in "${tuning_models[@]}"; do
+  echo -e "\n\nModel: ${tuning_model}"
+  for p in "${lambda_prediction[@]}"; do
+    for s in "${lambda_sparsity[@]}"; do
+      dir="lambda_pred_${p}-lambda_sparsity_${s}"
+      job_name="manual_tuning_tphystnd_691_${tuning_model}_lambda_pred_${p}-lambda_sparsity_${s}"
 
-    log_dir="${base_dir}/${tuning_model}/${dir}"
-    cfg="${log_dir}/cfg_castle_adapted.yml"
+      log_dir="${base_dir}/${trial}/${tuning_model}/${dir}"
+      cfg="${log_dir}/cfg_castle_adapted.yml"
 
-    slurm_o="${log_dir}/%x_slurm_%j.out"
-    slurm_e="${log_dir}/%x_error_slurm_%j.out"
+      slurm_o="${log_dir}/%x_slurm_%j.out"
+      slurm_e="${log_dir}/%x_error_slurm_%j.out"
 
-    echo -e "\nSubmitting job ${job_name}"
-    sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_castle_batch.sh -c "$cfg" -i "$in" -o "$out" -x "$idx" -p "$log_dir" -j "$job_name"
+      echo -e "\nSubmitting job ${job_name}"
+      sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_castle_batch.sh -c "$cfg" -i "$in" -o "$out" -x "$idx" -p "$log_dir" -j "$job_name"
+    done
   done
+  echo ""
 done
 echo ""
