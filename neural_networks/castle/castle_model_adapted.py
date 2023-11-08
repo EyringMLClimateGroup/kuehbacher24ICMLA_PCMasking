Bugfix: Fix wrong input layer weights computation; Remove output reshaping; Add DAGMA parameter s to CASTLEAdapted model parameters

neural_networks/castle/castle_model_adapted.py
@@ -99,6 +99,7 @@ class CASTLEAdapted(CASTLEBase):
                  lambda_reconstruction,
                  lambda_acyclicity,
                  acyclicity_constraint,
+                 dagma_s=1.0,
                  relu_alpha=0.3,
                  seed=None,
                  kernel_initializer_input_layers=None,
@@ -164,7 +165,7 @@ class CASTLEAdapted(CASTLEBase):
         # Stack outputs into one tensor
         # outputs = tf.squeeze(tf.stack(yx_outputs, axis=1))
         outputs = tf.stack(yx_outputs, axis=1, name="stack_outputs")
-        outputs = tf.reshape(outputs, shape=(tf.shape(outputs)[0], tf.shape(outputs)[1]), name="reshape_output_dims")
+        # outputs = tf.reshape(outputs, shape=(tf.shape(outputs)[0], tf.shape(outputs)[1]), name="reshape_output_dims")
 
         super(CASTLEAdapted, self).__init__(num_x_inputs=num_x_inputs, hidden_layers=hidden_layers,
                                             activation=activation, rho=rho, alpha=alpha, seed=seed,
@@ -199,6 +200,7 @@ class CASTLEAdapted(CASTLEBase):
         self.output_sub_layers = output_sub_layers
 
         self.acyclicity_constraint = acyclicity_constraint
+        self.dagma_s = dagma_s
         if acyclicity_constraint.upper() == "NOTEARS":
             self.acyclicity_constraint_func = compute_h_matrix_exp
         elif acyclicity_constraint.upper() == "DAGMA":
@@ -229,7 +231,7 @@ class CASTLEAdapted(CASTLEBase):
             is the case when called by `Model.test_step`).
         """
         input_layer_weights = [self.input_sub_layers[0].trainable_variables[0]]
-        input_layer_weights.extend([layer.trainable_variables[0] * layer.mask for layer in self.input_sub_layers[1:0]])
+        input_layer_weights.extend([layer.trainable_variables[0] * layer.mask for layer in self.input_sub_layers[1:]])
 
         # In CASTLE, y_pred is (y_pred, x_pred)
         prediction_loss = self.compute_prediction_loss(y, y_pred)
@@ -237,7 +239,8 @@ class CASTLEAdapted(CASTLEBase):
 
         reconstruction_loss = self.compute_reconstruction_loss_x(x, y_pred)
         weighted_reconstruction_loss = tf.math.multiply(self.lambda_reconstruction, reconstruction_loss)
-        acyclicity_loss = self.compute_acyclicity_loss(input_layer_weights, self.acyclicity_constraint_func)
+        acyclicity_loss = self.compute_acyclicity_loss(input_layer_weights, self.acyclicity_constraint_func,
+                                                       s=self.dagma_s)
         weighted_acyclicity_loss = tf.math.multiply(self.lambda_acyclicity, acyclicity_loss)
         sparsity_regularizer = self.compute_sparsity_loss(input_layer_weights)
         weighted_sparsity_regularizer = tf.math.multiply(self.lambda_sparsity, sparsity_regularizer)
@@ -316,7 +319,7 @@ class CASTLEAdapted(CASTLEBase):
         Overrides base method.
 
         `input_true` contains only x-values."""
-        return tf.metrics.mse(input_true, yx_pred[:, 1:])
+        return tf.metrics.mse(tf.expand_dims(input_true, axis=-1), yx_pred[:, 1:])
 
     def get_config(self):
         """Returns the config of `CASTLEAdapted`.
@@ -343,6 +346,7 @@ class CASTLEAdapted(CASTLEBase):
                 "lambda_reconstruction": self.lambda_reconstruction,
                 "lambda_acyclicity": self.lambda_acyclicity,
                 "acyclicity_constraint": self.acyclicity_constraint,
+                "dagma_s": self.dagma_s,
                 "relu_alpha": self.relu_alpha,
             }
         )

neural_networks/castle/castle_model_base.py
@@ -202,27 +202,29 @@ class CASTLEBase(tf.keras.Model, ABC):
     @staticmethod
     def compute_prediction_loss(y_true, yx_pred):
         """Computes CASTLE prediction loss."""
-        return tf.reduce_mean(tf.keras.losses.mse(y_true, yx_pred[:, 0]), name="prediction_loss_reduce_mean")
+        return tf.reduce_mean(tf.keras.losses.mse(tf.expand_dims(y_true, axis=-1), yx_pred[:, 0]),
+                              name="prediction_loss_reduce_mean")
 
     @staticmethod
     def compute_reconstruction_loss_x(x_true, yx_pred):
         """Computes CASTLE reconstruction loss."""
         # Frobenius norm between all inputs and outputs averaged over the number of samples in the batch
-        return tf.reduce_mean(tf.norm(tf.subtract(x_true, yx_pred[:, 1:]), ord='fro', axis=[-2, -1]),
-                              name="reconstruction_loss_reduce_mean")
+        return tf.reduce_mean(
+            tf.norm(tf.subtract(tf.expand_dims(x_true, axis=-1), yx_pred[:, 1:]), ord='fro', axis=[-2, -1]),
+            name="reconstruction_loss_reduce_mean")
 
     @staticmethod
     def compute_reconstruction_loss_yx(yx_true, yx_pred):
         """Computes CASTLE reconstruction loss."""
         # Frobenius norm between all inputs and outputs averaged over the number of samples in the batch
-        return tf.reduce_mean(tf.norm(tf.subtract(yx_true, yx_pred), ord='fro', axis=[-2, -1]),
+        return tf.reduce_mean(tf.norm(tf.subtract(tf.expand_dims(yx_true, axis=-1), yx_pred), ord='fro', axis=[-2, -1]),
                               name="reconstruction_loss_reduce_mean")
 
-    def compute_acyclicity_loss(self, input_layer_weights, acyclicity_loss_func):
+    def compute_acyclicity_loss(self, input_layer_weights, acyclicity_constraint_func, **kwargs):
         """Computes the Lagrangian optimization equation with the acyclicity constraint."""
         l2_norm_matrix = self.compute_l2_norm_matrix(input_layer_weights)
 
-        h = acyclicity_loss_func(l2_norm_matrix)
+        h = compute_acyclicity_constraint(acyclicity_constraint_func, l2_norm_matrix, **kwargs)
         # tf.print(f"h function = {h}")
 
         # Acyclicity loss is computed using the Lagrangian scheme with penalty parameter rho and
@@ -463,6 +465,37 @@ def _build_input_sub_layers_without_y(num_input_layers, num_x_inputs, units, act
                                               activity_regularizer=activity_regularizer_input_layers)
         input_sub_layers.append(masked_dense_layer)
     return input_sub_layers
+
+
+def compute_acyclicity_constraint(acyclicity_constraint_func, matrix, **kwargs):
+    """
+    Computes the acyclicity constraint for the given matrix with the
+    function `acyclicity_constraint_func`.
+    Keyword arguments for the `acyclicity_constraint_func` can be passed in `**kwargs`.
+
+    Args:
+        acyclicity_constraint_func (callable): Acyclicity constraint function.
+        matrix (2d np.array or tf.Tensor): Matrix to compute the acyclicity constraint for.
+        **kwargs: Keyword arguments for the acyclicity constraint function.
+
+    Returns:
+        Float value for acyclicity constraint.
+    """
+    if acyclicity_constraint_func is compute_h_matrix_exp:
+        try:
+            approximate = kwargs["approximate"]
+            h = compute_h_matrix_exp(matrix, approximate=approximate)
+        except KeyError:
+            h = compute_h_matrix_exp(matrix)
+    elif acyclicity_constraint_func is compute_h_log_det:
+        try:
+            s = kwargs["s"]
+            h = compute_h_log_det(matrix, s=s)
+        except KeyError:
+            h = compute_h_log_det(matrix)
+    else:
+        raise ValueError(f"Unknown acyclicity constraint function: {acyclicity_constraint_func}.")
+    return h
 
 
 def compute_h_matrix_exp(matrix, approximate=True):

neural_networks/castle/castle_model_original.py
@@ -146,7 +146,7 @@ class CASTLEOriginal(CASTLEBase):
         # Stack outputs into one tensor
         # outputs = tf.squeeze(tf.stack(yx_outputs, axis=1))
         outputs = tf.stack(yx_outputs, axis=1, name="stack_outputs")
-        outputs = tf.reshape(outputs, shape=(tf.shape(outputs)[0], tf.shape(outputs)[1]), name="reshape_output_dims")
+        # outputs = tf.reshape(outputs, shape=(tf.shape(outputs)[0], tf.shape(outputs)[1]), name="reshape_output_dims")
 
         super(CASTLEOriginal, self).__init__(num_x_inputs=num_x_inputs, hidden_layers=hidden_layers,
                                              activation=activation, rho=rho, alpha=alpha, seed=seed,
@@ -272,7 +272,7 @@ class CASTLEOriginal(CASTLEBase):
         Overrides base method.
 
         `input_true` contains both y and x-values."""
-        return tf.metrics.mse(input_true[:, 1:], yx_pred[:, 1:])
+        return tf.metrics.mse(tf.expand_dims(input_true[:, 1:], axis=-1), yx_pred[:, 1:])
 
     def get_config(self):
         """Returns the config of `CASTLEOriginal`.