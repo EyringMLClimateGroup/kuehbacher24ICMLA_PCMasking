44e5c958357dff0d1f3b5b8a8a1fb64d5e05ba9c
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2023-11-20 15:06:58 +0100

    Manual tuning test run

--- manual_tuning_wrapper.sh
+++ manual_tuning_wrapper.sh
@@ -34,11 +34,11 @@ done
 shift "$(($OPTIND - 1))"
 
 # Set tuning parameters
-lambda_prediction=(1 2 4 8 10)
-lambda_sparsity=(0.1 0.5 1.0)
+lambda_prediction=(1 10 100) #(1 2 4 8 10)
+lambda_sparsity=(1.0) # (0.1 0.5 1.0)
 
 # Set base directory for config files and inputs/outputs list files
-base_dir="output_castle/manual_tuning_tphystnd_691.39_v3"
+base_dir="output_castle/manual_tuning_tphystnd_691.39_v3-test"
 tuning_models=("castle_adapted_big_notears")
 #tuning_models=("castle_adapted_small_dagma" "castle_adapted_big_dagma" "castle_adapted_small_notears" "castle_adapted_big_notears")
 in="${base_dir}/inputs_list.txt"
