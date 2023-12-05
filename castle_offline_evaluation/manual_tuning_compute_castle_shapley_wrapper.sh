955da024cc67d926559125c1e42c4c89f4d5becd
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2023-12-05 08:44:05 +0100

    New tuning/SHAP computation runs; Set variable index dynamically

--- castle_offline_evaluation/manual_tuning_compute_castle_shapley_wrapper.sh
+++ castle_offline_evaluation/manual_tuning_compute_castle_shapley_wrapper.sh
@@ -5,21 +5,32 @@
 ##############################
 
 # Set tuned parameters
-lambda_prediction=(1 10 100 1000 10000) #(1 2 4 8 10)
+lambda_prediction=(1 5 10 100 1000) #(1 2 4 8 10)
 lambda_sparsity=(1.0)                     #(0.1 0.5 1.0)
 
 PROJECT_ROOT="$(dirname "${PWD}")"
 
 # Set base directory for config files and inputs/outputs list files
-base_dir="${PROJECT_ROOT}/output_castle/manual_tuning_tphystnd_820.86_v3" # manual_tuning_tphystnd_820.86_v3, manual_tuning_tphystnd_691.39_v3
+base_dir="${PROJECT_ROOT}/output_castle/manual_tuning_tphystnd_691.39_v5-transposed_l1_entry_scaled_init_he" # manual_tuning_tphystnd_820.86_v3, manual_tuning_tphystnd_691.39_v3
+var="tphystnd_691.39" # tphystnd_691.39, tphystnd_820.86
+job_prefix="transposed_l1_entry_scaled_init_he"
+
 tuning_models=("castle_adapted_big_notears")                                    #("castle_adapted_small_dagma" "castle_adapted_big_dagma" "castle_adapted_small_notears" "castle_adapted_big_notears")
 in="${base_dir}/inputs_list.txt"
 out="${base_dir}/outputs_list.txt"
 map="${base_dir}/outputs_map.txt"
-var="tphystnd_820.86" # tphystnd_691.39
 
 # Index for network to be tuned: variable tphystnd-691.39
-idx="22" # tphystnd_691.39 20, tphystnd_820.86 22
+# Index for network to be tuned:
+# tphystnd_691.39 20, tphystnd_820.86 22
+if [[ $var == "tphystnd_691.39" ]]; then
+  idx="20"
+elif [[ $var == "tphystnd_820.86" ]]; then
+  idx="22"
+else
+  echo -e "\nIndex for variable ${var} not known. Exiting script.\n"
+  exit 1
+fi
 
 #######################################
 # Set parameters for SHAP computation #
@@ -70,11 +81,13 @@ shift "$(($OPTIND - 1))"
 # Start jobs #
 ##############
 
+echo -e "\n\nBase directory: ${base_dir}\n"
+
 for tuning_model in "${tuning_models[@]}"; do
   for p in "${lambda_prediction[@]}"; do
     for s in "${lambda_sparsity[@]}"; do
       dir="lambda_pred_${p}-lambda_sparsity_${s}"
-      job_name="compute_shap_${var}_${tuning_model}_la_pred-${p}_la_sparsity-${s}"
+      job_name="shap_${job_prefix}_${var}_${tuning_model}_la_pred-${p}_la_sparsity-${s}"
 
       log_dir="${base_dir}/${tuning_model}/${dir}"
       plot_dir="${base_dir}/evaluation/${tuning_model}/${dir}/shap"
@@ -86,6 +99,7 @@ for tuning_model in "${tuning_models[@]}"; do
       slurm_e="${plot_dir}/%x_error_slurm_%j.out"
 
       echo -e "\nSubmitting job ${job_name}"
+      echo -e "Index: ${idx}"
       sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_compute_castle_shapley.sh -c "$cfg" -o "$out" -x $idx -m "$map" -p "$plot_dir" -t "$n_time" -s "$n_samples" -e "$metric" -l "$plot_dir" -j "$job_name"
     done
   done
--- manual_tuning_wrapper.sh
+++ manual_tuning_wrapper.sh
@@ -34,26 +34,36 @@ done
 shift "$(($OPTIND - 1))"
 
 # Set tuning parameters
-lambda_prediction=(1 10 100 1000 10000) #(1 2 4 8 10)
-lambda_sparsity=(1.0) # (0.1 0.5 1.0)
+lambda_prediction=(1 5 10 100 1000) #(1 2 4 8 10)
+lambda_sparsity=(1.0)               # (0.1 0.5 1.0)
 
 # Set base directory for config files and inputs/outputs list files
-base_dir="output_castle/manual_tuning_tphystnd_820.86_v3"  # manual_tuning_tphystnd_691.39_v3-test2
+base_dir="output_castle/manual_tuning_tphystnd_691.39_v4-transposed_l1_entry_scaled" # manual_tuning_tphystnd_691.39_v3-test2
+var="tphystnd_691.39"                                                                # tphystnd_691.39, tphystnd_820.86
+job_prefix="v4-transposed_l1_entry_scaled"
+
 tuning_models=("castle_adapted_big_notears")
-#tuning_models=("castle_adapted_small_dagma" "castle_adapted_big_dagma" "castle_adapted_small_notears" "castle_adapted_big_notears")
+# tuning_models=("castle_adapted_small_dagma" "castle_adapted_big_dagma" "castle_adapted_small_notears" "castle_adapted_big_notears")
 in="${base_dir}/inputs_list.txt"
 out="${base_dir}/outputs_list.txt"
-var="tphystnd_820.86"  # tphystnd_691.39
 
-# Index for network to be tuned: variable tphystnd-691.39
-idx="22-22" # tphystnd_691.39 20, tphystnd_820.86 22
+# Index for network to be tuned:
+# tphystnd_691.39 20, tphystnd_820.86 22
+if [[ $var == "tphystnd_691.39" ]]; then
+  idx="20-20"
+elif [[ $var == "tphystnd_820.86" ]]; then
+  idx="22-22"
+else
+  echo -e "\nIndex for variable ${var} not known. Exiting script.\n"
+  exit 1
+fi
 
 for tuning_model in "${tuning_models[@]}"; do
   echo -e "\n\nModel: ${tuning_model}"
   for p in "${lambda_prediction[@]}"; do
     for s in "${lambda_sparsity[@]}"; do
       dir="lambda_pred_${p}-lambda_sparsity_${s}"
-      job_name="manual_tuning_${var}_${tuning_model}_lpred_${p}-lsparsity_${s}"
+      job_name="${job_prefix}_${var}_${tuning_model}_lpred_${p}-lsparsity_${s}"
 
       log_dir="${base_dir}/${tuning_model}/${dir}"
       cfg="${log_dir}/cfg_castle_adapted.yml"
@@ -62,7 +72,8 @@ for tuning_model in "${tuning_models[@]}"; do
       slurm_e="${log_dir}/%x_error_slurm_%j.out"
 
       echo -e "\nSubmitting job ${job_name}"
-      sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_castle_batch.sh -c "$cfg" -i "$in" -o "$out" -x "$idx" -p "$log_dir" -j "$job_name"
+      echo "Index: ${idx}"
+#      sbatch --job-name "$job_name" --output "$slurm_o" --error "$slurm_e" manual_tuning_castle_batch.sh -c "$cfg" -i "$in" -o "$out" -x "$idx" -p "$log_dir" -j "$job_name"
     done
   done
   echo ""
