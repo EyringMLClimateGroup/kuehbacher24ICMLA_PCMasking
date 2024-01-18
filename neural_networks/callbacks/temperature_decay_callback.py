d5d2a7b77ca6885a98d284e21d78180e95734b70
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2024-01-18 16:47:55 +0100

    Include temperature decay warm up

--- neural_networks/callbacks/temperature_decay_callback.py
+++ neural_networks/callbacks/temperature_decay_callback.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 
 
 class TemperatureDecay(keras.callbacks.Callback):
-    def __init__(self, initial_temperature, decay_rate, decay_steps, tb_log_dir=""):
+    def __init__(self, initial_temperature, decay_rate, decay_steps, warm_up=0, tb_log_dir=""):
         super().__init__()
         self.initial_temperature = initial_temperature
         print(f"\n\nInitial temperature = {self.initial_temperature}\n")
@@ -13,8 +13,11 @@ class TemperatureDecay(keras.callbacks.Callback):
         self.decay_rate = decay_rate
         self.decay_steps = decay_steps
 
+        self.warm_up = warm_up
+
         print(f"Decay rate = {self.decay_rate}")
-        print(f"Decay steps = {self.decay_steps}\n\n")
+        print(f"Decay steps = {self.decay_steps}")
+        print(f"Temperature warm up = {self.warm_up}\n\n")
 
         self.tb_log_dir = tb_log_dir
         self.tb_logging_active = False
@@ -39,4 +42,7 @@ class TemperatureDecay(keras.callbacks.Callback):
             tf.summary.scalar(name="mask_sum", data=mask_sum, step=epoch)
 
     def temperature_decay(self, step):
-        return self.initial_temperature * tf.math.pow(self.decay_rate, (step / self.decay_steps))
+        if step >= self.warm_up:
+            return self.initial_temperature * tf.math.pow(self.decay_rate, ((step - self.warm_up) / self.decay_steps))
+        else:
+            return self.initial_temperature
