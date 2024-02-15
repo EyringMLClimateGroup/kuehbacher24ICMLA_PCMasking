e05a6db37b18f802bdffd90df010f32a0c32db42
Birgit Kuehbacher <birgit.kuehbacher@dlr.de>
2024-02-15 14:30:17 +0100

    Run new training; Reduce number of cpus in slurm jobs

--- train_castle_split_nodes_gpu_jsc_wrapper.sh
+++ train_castle_split_nodes_gpu_jsc_wrapper.sh
@@ -4,15 +4,15 @@
 # Default argument values #
 ###########################
 # todo: extract base dir from config?
-base_dir="output_castle/training_66_castle_simplified_spars0.001"
+base_dir="output_castle/training_69_vector_mask_net_minus4K"
 HPC="gpu_jsc" # jsc, dkrz, gpu_jsc
 
-job_name_base="training_66_castle_simplified_spars0.001"
+job_name_base="training_69_vector_mask_net_minus4K_t0.9"
 
 NN_INPUTS="${base_dir}/inputs_list.txt"
 NN_OUTPUTS="${base_dir}/outputs_list.txt"
 OUTPUTS_MAP="${base_dir}/outputs_map.txt"
-NN_CONFIG="${base_dir}/cfg_castle_simplified.yml"
+NN_CONFIG="${base_dir}/cfg_vector_mask_net_t0.9.yml"
 
 NUM_NODES=17
 NN_PER_NODE=4
@@ -22,7 +22,7 @@ CONTINUE_TRAINING="False"
 
 MAX_RUNNING_JOBS_DKRZ=20
 
-log_dir="${base_dir}/slurm_logs"
+log_dir="${base_dir}/slurm_logs_0.9"
 mkdir -p "$log_dir"
 
 #############
